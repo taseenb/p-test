5ec0d67a050a5e0a9542af594b77f2bcc93543b9
taseenb <esteban.almiron@gmail.com>
2015-04-09 23:31:07 +0100

    scaffolding

---
+++ js/app.js
@@ -0,0 +1,6 @@
+(function () {var src_scripts_app = function () {
+        'use strict';
+        console.log('start app!!!');
+        console.log($('body'));
+    }();
+}());
---
+++ src/scripts/app.js
@@ -0,0 +1,7 @@
+define([], function() {
+  'use strict';
+
+  console.log('start app!!!');
+  console.log($('body'));
+
+});
